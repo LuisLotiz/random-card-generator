72fbf8a213582d2ed4c89332ba635c0fbef9c0fb
Luis López Ortiz <96153356+LuisLotiz@users.noreply.github.com>
2023-07-16 20:11:21 +0000

    JS finalizado y operativo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = randomNumber();
+  document.querySelector("#suits").innerHTML = suit;
+  document.querySelector("#suits1").innerHTML = suit;
+  document.querySelector(".bottomSuit").classList.add(suitColor());
+  document.querySelector(".topSuit").classList.add(suitColor());
+};
+
+const randomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let index = Math.floor(Math.random() * numbers.length);
+  return numbers[index];
+};
+
+const randomSuit = () => {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let index = Math.floor(Math.random() * suits.length);
+  return suits[index];
+};
+
+const suit = randomSuit();
+
+const suitColor = () => {
+  const color = suit === "♦" || suit === "♥" ? "red" : "black";
+  return color;
 };
